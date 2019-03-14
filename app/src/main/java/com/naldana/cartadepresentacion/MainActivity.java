f981e82dcbd37add79af6c4d5d2bebc0fc1ab86a
danielorozco14 <00200617@uca.edu.sv>
2019-03-14 17:05:57 -0600

    Individual terminado

---
+++ app/src/main/java/com/naldana/cartadepresentacion/MainActivity.java
@@ -0,0 +1,23 @@
+package com.naldana.cartadepresentacion;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.naldana.ejemplo04.R;
+
+import java.util.zip.Inflater;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
